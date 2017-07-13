e7473aa91e71e56dded05623fccfa5ce01ee234c
SkalskiP <pskalski@student.agh.edu.pl>
2017-07-13 22:26:35 +0200

    First connection to DB

--- music/urls.py
+++ music/urls.py
@@ -5,7 +5,8 @@ urlpatterns = [
     # /music/
     url(r'^$', views.index, name='index'),
 
-    # /music/album_id
-    url(r'^$'),
+    # /music/album_id/
+    # album_id may be used as view argument
+    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
 
 ]
