feea4466cddc254123b0623bbf88ed92283bb348
SkalskiP <pskalski@student.agh.edu.pl>
2017-07-15 01:00:04 +0200

    Test code after 19 episodes of Tutorial

--- music/models.py
+++ music/models.py
@@ -14,3 +14,6 @@ class Song(models.Model):
     # models.CASCADE whenever we delete album, any song that had any link to that, we delete them
     file_type = models.CharField(max_length = 10)
     song_title = models.CharField(max_length = 250)
+
+    def __str__(self):
+        return self.song_title
